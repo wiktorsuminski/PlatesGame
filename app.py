285f65641b1aa27f42f7ce267c272066c054e9b4
Shiraoku <suminskiwiktor@gmail.com>
2021-10-21 20:51:33 +0200

    added all districts

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import random
 import string
 
 app = Flask(__name__)
+
 app.secret_key = "TestowyKlucz"
-random.seed(1)
+
+random.seed(random.SystemRandom)
 plateDistricts = {'BI': 'białystok', 'BS': 'suwałki', 'BL': 'łomża', 'BAU': 'powiat augustowski', 'BIA': 'powiat białostocki', 'BBI': 'powiat bielski',
                   'BGR': 'powiat grajewski', 'BHA': 'powiat hajnowski', 'BKL': 'powiat kolneński', 'BMN': 'powiat moniecki', 'BSE': 'powiat sejneński',
                   'BSI': 'powiat siemiatycki', 'BSK': 'powiat sokólski', 'BSU': 'powiat suwalski', 'BWM': 'powiat wysokomazowiecki', 'BZA': 'powiat zambrowski',
@@ -18,8 +20,60 @@ plateDistricts = {'BI': 'białystok', 'BS': 'suwałki', 'BL': 'łomża', 'BAU': 
                   'DKL': 'powiat kłodzki', 'DLE': 'powiat legnicki', 'DLB': 'powiat lubański', 'DLU': 'powiat lubiński', 'DLW': 'powiat lwówecki', 'DMI': 'powiat milicki',
                   'DOL': 'powiat oleśnicki', 'DOA': 'powiat oławski', 'DPL': 'powiat polkowicki', 'DSR': 'powiat średzki', 'DST': 'powiat strzeliński', 'DSW': 'powiat świdnicki',
                   'DTR': 'powiat trzebnicki', 'DBA': 'powiat wałbrzyski', 'DWL': 'powiat wołowski', 'DWR': 'powiat wrocławski', 'DZA': 'powiat ząbkowicki', 'DZG': 'powiat zgorzelecki',
-                  'DZL': 'powiat złotoryjski'
-
+                  'DZL': 'powiat złotoryjski', 'EP': 'piotrków trybunalski', 'ES': 'skierniewice', 'EL': 'łódź', 'EBE': 'powiat bełchatowski', 'EBR': 'powiat brzeziński',
+                  'EKU': 'powiat kutnowski', 'EOP': 'powiat opoczyński', 'EPA': 'powiat pabianicki', 'EPJ': 'powiat pajęczański', 'EPI': 'powiat piotrkowski', 'EPD': 'powiat poddębicki',
+                  'ERA': 'powiat radomszczański', 'ERW': 'powiat rawski', 'ESI': 'powiat sieradzki', 'ESK': 'powiat skierniewicki', 'ETM': 'powiat tomaszowski', 'EWI': 'powiat wieluński',
+                  'EWE': 'powiat wieruszowski', 'EZD': 'powiat zduńskowolski', 'EZG': 'powiat zgierski', 'ELA': 'powiat łaski', 'ELE': 'powiat łęczycki', 'ELW': 'powiat łódzki wschodni',
+                  'ELC': 'powiat łowicki', 'FG': 'gorzów wielkopolski', 'FZ': 'zielona góra', 'FGW': 'powiat gorzowski', 'FKR': 'powiat krośnieński', 'FMI': 'powiat międzyrzecki',
+                  'FNW': 'powiat nowosolski', 'FSD': 'powiat strzelecko-drezdenecki', 'FSU': 'powiat sulęciński', 'FSW': 'powiat świebodziński', 'FSL': 'powiat słubicki',
+                  'FWS': 'powiat wschowski', 'FZG': 'powiat żagański', 'FZA': 'powiat żarski', 'FZI': 'powiat zielonogórski', 'GD': 'gdańsk', 'GA': 'gdynia', 'GSP': 'sopot',
+                  'GS': 'słupsk', 'GBY': 'powiat bytowski', 'GCH': 'powiat chojnicki', 'GCZ': 'powiat człuchowski', 'GDA': 'powiat gdański', 'GKA': 'powiat kartuski',
+                  'GKS': 'powiat kościerski', 'GKW': 'powiat kwidzyński', 'GLE': 'powiat lęborski', 'GMB': 'powiat malborski', 'GND': 'powiat nowodworski', 'GPU': 'powiat pucki',
+                  'GST': 'powiat starogardzki', 'GSZ': 'powiat sztumski', 'GSL': 'powiat słupski', 'GTC': 'powiat tczewski', 'GWE': 'powiat wejherowski', 'GWO': 'powiat wejherowski',
+                  'KR': 'kraków', 'KK': 'kraków', 'KN': 'nowy sącz', 'KT': 'tarnów', 'KBC': 'powiat bocheński', 'KBA': 'powiat bocheński', 'KBR': 'powiat brzeski', 'KCH': 'powiat chrzanowski',
+                  'KDA': 'powiat dąbrowski', 'KGR': 'powiat gorlicki', 'KRA': 'powiat krakowski', 'KLI': 'powiat limanowski', 'KMI': 'powiat miechowski', 'KMY': 'powiat myślenicki',
+                  'KNS': 'powiat nowosądecki', 'KNT': 'powiat nowotarski', 'KOL': 'powiat olkuski', 'KOS': 'powiat oświęcimski', 'KPR': 'powiat proszowicki', 'KSU': 'powiat suski',
+                  'KTA': 'powiat tarnowski', 'KTT': 'powiat tatrzański', 'KWA': 'powiat wadowicki', 'KWI': 'powiat wielicki', 'LB': 'biała podlaska', 'LC': 'chełm', 'LU': 'lublin',
+                  'LZ': 'zamość', 'LBI': 'powiat bialski', 'LBL': 'powiat biłgorajski', 'LCH': 'powiat chełmski', 'LHR': 'powiat hrubieszowski', 'LJA': 'powiat janowski',
+                  'LKR': 'powiat kraśnicki', 'LKS': 'powiat krasnostawski', 'LLB': 'powiat lubartowski', 'LUB': 'powiat lubelski', 'LOP': 'powiat opolski', 'LPA': 'powiat parczewski',
+                  'LPU': 'powiat puławski', 'LRA': 'powiat radzyński', 'LRY': 'powiat rycki', 'LSW': 'powiat świdnicki', 'LTM': 'powiat tomaszowski', 'LWL': 'powiat włodawski',
+                  'LZA': 'powiat zamojski', 'LLE': 'powiat łęczyński', 'LLU': 'powiat łukowski', 'NE': 'elbląg', 'NO': 'olsztyn', 'NBA': 'powiat bartoszycki', 'NBR': 'powiat braniewski',
+                  'NDZ': 'powiat działdowski', 'NEB': 'powiat elbląski', 'NEL': 'powiat ełcki', 'NGI': 'powiat giżycki', 'NGO': 'powiat gołdapski', 'NIL': 'powiat iławski',
+                  'NKE': 'powiat kętrzyński', 'NLI': 'powiat lidzbarski', 'NMR': 'powiat mrągowski', 'NNI': 'powiat nidzicki', 'NNM': 'powiat nowomiejski', 'NOE': 'powiat olecki',
+                  'NOL': 'powiat olsztyński', 'NOS': 'powiat ostródzki', 'NPI': 'powiat piski', 'NSZ': 'powiat szczycieński', 'NWE': 'powiat węgorzewski',
+                  'OP': 'opole', 'OB': 'powiat brzeski', 'OGL': 'powiat głubczycki', 'OK': 'powiat kędzierzyńsko-kozielski', 'OKL': 'powiat kluczborski', 'OKR': 'powiat krapkowicki',
+                  'ONA': 'powiat namysłowski', 'ONY': 'powiat nyski', 'OOL': 'powiat oleski', 'OPO': 'powiat opolski', 'OPR': 'powiat prudnicki', 'OST': 'powiat strzelecki',
+                  'PK': 'kalisz', 'PN': 'konin', 'PL': 'leszno', 'PO': 'poznań', 'PY': 'poznań', 'PCH': 'powiat chodzieski', 'PCT': 'powiat czarnkowsko-trzcianecki', 'PGN': 'powiat gnieźnieński',
+                  'PGS': 'powiat gostyński', 'PGO': 'powiat grodziski', 'PJA': 'powiat jarociński', 'PKA': 'powiat kaliski', 'PKE': 'powiat kępiński', 'PKL': 'powiat kolski',
+                  'PKN': 'powiat koniński', 'PKS': 'powiat kościański', 'PKR': 'powiat krotoszyński', 'PLE': 'powiat leszczyński', 'PMI': 'powiat międzychodzki', 'PNT': 'powiat nowotomyski',
+                  'POB': 'powiat obornicki', 'POS': 'powiat ostrowski', 'POT': 'powiat otrzeszowski', 'PP': 'powiat pilski', 'PPL': 'powiat pleszewski', 'PZ': 'powiat poznański',
+                  'PRA': 'powiat rawicki', 'PSR': 'powiat średzki', 'PSE': 'powiat śremski', 'PSZ': 'powiat szamotulski', 'PSL': 'powiat słupecki', 'PTU': 'powiat turecki',
+                  'PWA': 'powiat wągrowiecki', 'PWL': 'powiat wolsztyński', 'PWR': 'powiat wrzesiński', 'PZL': 'powiat złotowski', 'PK': 'krosno', 'RP': 'przemyśl',
+                  'RZ': 'rzeszów', 'RT': 'tarnobrzeg', 'RBI': 'powiat bieszczadzki', 'RBR': 'powiat brzozowski', 'RDE': 'powiat dębicki', 'RJA': 'powiat jarosławski',
+                  'RJS': 'powiat jasielski', 'RKL': 'powiat kolbuszowski', 'RKR': 'powiat krośnieński', 'RLS': 'powiat leski', 'RLE': 'powiat leżajski', 'RLU': 'powiat lubaczowski',
+                  'RMI': 'powiat mielecki', 'RNI': 'powiat niżański', 'RPR': 'powiat przemyski', 'RPZ': 'powiat przeworski', 'RRS': 'powiat ropczycko-sędziszowski', 'RZE': 'powiat rzeszowski',
+                  'RSA': 'powiat sanocki', 'RST': 'powiat stalowowolski', 'PSR': 'Powiat strzyżowski', 'RTA': 'powiat tarnobrzeski', 'RLA': 'powiat łańcucki', 'SB': 'bielsko-biała',
+                  'SY': 'bytom', 'SH': 'chorzów', 'SC': 'częstochowa', 'SD': 'dąbrowa górnicza', 'SG': 'gliwice', 'SJZ': 'jastrzębie-zdrój', 'SJ': 'jaworzno', 'SK': 'katowice', 'SM': 'mysłowice',
+                  'SPI': 'piekary śląskie', 'SL': 'ruda śląska', 'SR': 'rybnik', 'SI': 'siemianowice śląskie', 'SO': 'sosnowiec', 'SW': 'świętochłowice', 'ST': 'tychy', 'SZ': 'zabrze', 'SZO': 'żory',
+                  'SBE': 'powiat będziński', 'SBI': 'powiat bielski', 'SBL': 'powiat bieruńsko-lędziński', 'SCI': 'powiat cieszyński', 'SCZ': 'powiat częstochowski', 'SGL': 'powiat gliwicki',
+                  'SKL': 'powiat kłobucki', 'SLU': 'powiat lubliniecki', 'SMI': 'powiat mikołowski', 'SMY': 'powiat myszkowski', 'SPS': 'powiat pszczyński', 'SRC': 'powiat raciborski',
+                  'SRB': 'powiat rybnicki', 'STA': 'powiat tarnogórski', 'SWD': 'powiat wodzisławski', 'SWZ': 'powiat wodzisławski', 'SZA': 'powiat zawierciański', 'SZY': 'powiat żywiecki',
+                  'TK': 'kielce', 'TBU': 'powiat buski', 'TJE': 'powiat jędrzejowski', 'TKA': 'powiat kazimierski', 'TKI': 'powiat kielecki', 'TKN': 'powiat konecki', 'TOP': 'powiat opatowski',
+                  'TOS': 'powiat ostrowiecki', 'TPI': 'powiat pińczowski', 'TSA': 'powiat sandomierski', 'TSK': 'powiat skarżyski', 'TST': 'powiat starachowicki', 'TSZ': 'powiat staszowski',
+                  'TLW': 'powiat włoszczowski', 'WO': 'ostrołęka', 'WP': 'płock', 'WR': 'radom', 'WS': 'siedlce', 'WB': 'warszawa bemowo', 'WA': 'warszawa białołęka', 'WD': 'warszawa bielany',
+                  'WE': 'warszawa mokotów', 'WU': 'warszawa ochota', 'WH': 'warszawa praga północ', 'WF': 'warszawa praga południe', 'WW': ['warszawa rembertów', 'warszawa wilanów', 'warszawa włochy'],
+                  'WI': 'warszawa śródmieście', 'WJ': 'warszawa targówek', 'WK': 'warszawa ursus', 'WN': 'warszawa ursynów', 'WT': 'warszawa wawer', 'WX': ['warszawa wesoła', 'warszawa żoliborz'],
+                  'WY': 'warszawa wola', 'WBR': 'powiat białobrzeski', 'WCI': 'powiat ciechanowski', 'WG': 'powiat garwoliński', 'WGS': 'powiat gostyniński', 'WGM': 'powiat grodzicki',
+                  'WGR': 'powiat grójecki', 'WKZ': 'powiat kozienicki', 'WL': 'powiat legionowski', 'WLI': 'powiat lipski', 'WMA': 'powiat makowski', 'WM': 'powiat miński', 'WML': 'powiat mławski',
+                  'WND': 'powiat nowodworski', 'WOR': 'powiat ostrowski', 'WOS': 'powiat ostrołęcki', 'WOT': 'powiat otwocki', 'WPI': 'powiat pasieczyński', 'WPR': 'powiat pruszkowski',
+                  'WPP': 'powiat pruszkowski', 'WPS': 'powiat pruszkowski', 'WPZ': 'powiat przasnyski', 'WPY': 'powiat przysuski', 'WPU': 'powiat pułtuski', 'WPL': 'powiat płocki',
+                  'WPN': 'powiat płoński', 'WRA': 'powiat radomski', 'WSI': 'powiat siedlecki', 'WSE': 'powiat serpecki', 'WSC': 'powiat sochaczewski', 'WSK': 'powiat sokołowski',
+                  'WSZ': 'powiat szydłowiecki', 'WZ': 'powiat warszawski zachodni', 'WWE': 'powiat węgrowski', 'WWL': 'powiat wołomiński', 'WV': 'powiat wołomiński',
+                  'WWY': 'powiat wyszkowski', 'WZU': 'powiat żuromiński', 'WZW': 'powiat zwoleński', 'WZY': 'powiat żyrardowski', 'WLS': 'powiat łosicki', 'ZK': 'koszalin',
+                  'ZSW': 'świnoujście', 'ZS': 'szczecin', 'ZZ': 'szczecin', 'ZBI': 'powiat białogardzki', 'ZCH': 'powiat choszczeńśki', 'ZDR': 'powiat drawski', 'ZGL': 'powiat goleniowski',
+                  'ZGY': 'powiat gryficki', 'ZGR': 'powiat gryfiński', 'ZKA': 'powiat kamieński', 'ZKO': 'powiat koszaliński', 'ZKL': 'powiat kołobrzeski', 'ZMY': 'powiat myśliborski',
+                  'ZPL': 'powiat policki', 'ZPY': 'powiat pyrzycki', 'ZST': 'powiat stargardzki', 'ZSD': 'powiat świdwiński', 'ZSZ': 'powiat szczecinecki', 'ZSL': 'powiat sławieński',
+                  'ZWA': 'powiat wałecki', 'ZLO': 'powiat łobeski'
                   }
 
 
@@ -32,10 +86,11 @@ score = int()
 
 @app.route('/', methods=["GET", "POST"])
 def home():
+    print(len(plateDistricts.items()))
     global score
     lastPlateDistrict = session.get('lastPlateDistrict', None)
     if request.form.get('userAnswer'):
-        userAnswer = request.form.get('userAnswer')
+        userAnswer = request.form.get('userAnswer').lower()
         if lastPlateDistrict != None:
             if userAnswer == plateDistricts[lastPlateDistrict.split(' ', 1)[0]]:
                 userAnswer = 'Dobrze'
@@ -54,8 +109,8 @@ def home():
         session['lastPlateDistrict'] = newPlate
         return render_template('index.html', newPlate=newPlate, userAnswer=userAnswer, score=score, newPlateDistrict=newPlateDistrict)
     else:
-        wynik = "Koniec gry"
-        newPlate = wynik
+        result = "Koniec gry"
+        newPlate = result
         return render_template('index.html', newPlate=newPlate, userAnswer=userAnswer, score=score)
 
 
